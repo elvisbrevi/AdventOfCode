eb00c7fa08786cb07235dc8d9c001fcdd9b9a82d
Elvis Brevi <elvis.brevi@sag.gob.cl>
2023-12-07 08:58:26 -0300

    refactor: add original input to day1 puzzle

--- 2015/day-1/src/main.rs
+++ 2015/day-1/src/main.rs
@@ -1,8 +1,11 @@
 use std::collections::HashMap;
 
+use crate::input::get_input;
+mod input;
+
 fn main() {
     let posible_values = HashMap::from([('(', 1), (')', -1)]);
-    let input = "()(((()))(()()()())))))(((()()(((()))(()()()((((()";
+    let input = get_input();
     let mut floor = 0;
     let mut index = 1;
     let mut character_position = 0;
