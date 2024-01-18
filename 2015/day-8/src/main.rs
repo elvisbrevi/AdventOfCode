31e43d60db7a8808f467167b48b0726030f8da3c
Elvis Brevi <elvisbrevi@gmail.com>
2024-01-18 09:27:43 -0300

    feat: part one solution

--- 2015/day-8/src/main.rs
+++ 2015/day-8/src/main.rs
@@ -1,36 +1,22 @@
-use std::fs;
+use regex::Regex;
 
 fn main() {
-    let contents = fs::read_to_string("input.txt").unwrap();
+    let contents = include_str!("../input.txt");
     let lines: Vec<&str> = contents.split_whitespace().collect();
     let mut char_string_literals = 0;
     let mut char_in_memory = 0;
 
+    let regex = Regex::new(r"\\x[a-f0-9A-F]{2}").unwrap();
+
     for line in lines {
         char_string_literals += line.len();
 
-        let mut memory = format!("{:0}", &line[1..line.len() - 1])
-            .replace("\\\"", "\"")
-            .replace("\\\\", "\\");
-
-        // when is a character
-        let memory_clone = memory.clone();
-        let matches: Vec<&str> = memory_clone.matches("\\x").collect();
-
-        for char in matches {
-            let hex_value_index = &memory.find(char).unwrap(); //&memory[index + 2..index + 4];
-            let hex_value_str = &memory[hex_value_index + 2..hex_value_index + 4];
-
-            if let Ok(hex_value) = u8::from_str_radix(hex_value_str, 16) {
-                let character = char::from_u32(hex_value as u32).unwrap();
-                memory = String::from(&memory.replace(
-                    &memory[hex_value_index + 0..hex_value_index + 4],
-                    String::from(character).as_str(),
-                ));
-            }
-        }
-
-        char_in_memory += memory.len();
+        char_in_memory += regex
+            .replace_all(line, "x")
+            .replace("\\\\", "x")
+            .replace("\\\"", "x")
+            .len()
+            - 2;
     }
 
     println!("{}", char_string_literals);
