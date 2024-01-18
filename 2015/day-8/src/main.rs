9da73b4525e88a62f7a85c72e1efccce5adcc732
Elvis Brevi <elvisbrevi@Elviss-MacBook-Air.local>
2024-01-18 07:09:00 -0300

    incorrect but nice

--- 2015/day-8/src/main.rs
+++ 2015/day-8/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 fn main() {
-    let contents = fs::read_to_string("input-test.txt").unwrap();
+    let contents = fs::read_to_string("input.txt").unwrap();
     let lines: Vec<&str> = contents.split_whitespace().collect();
     let mut char_string_literals = 0;
     let mut char_in_memory = 0;
@@ -18,19 +18,15 @@ fn main() {
         let matches: Vec<&str> = memory_clone.matches("\\x").collect();
 
         for char in matches {
-            let hex_value_str = &memory.find("\\x"); //&memory[index + 2..index + 4];
-            if let Ok(hex_value) = u8::from_str_radix(hex_value_str, 16) {
-                if let Some(character) = char::from_u32(hex_value as u32) {
-                    memory = String::from(
-                        &memory
-                            .replace(&memory[index..index + 4], String::from(character).as_str()),
-                    );
+            let hex_value_index = &memory.find(char).unwrap(); //&memory[index + 2..index + 4];
+            let hex_value_str = &memory[hex_value_index + 2..hex_value_index + 4];
 
-                    println!("memory: {}", memory);
-                    println!("memory clone: {}", memory_clone);
-                    // println!("valor: {} a", memory);
-                    // println!("result: {}", memory.len());
-                }
+            if let Ok(hex_value) = u8::from_str_radix(hex_value_str, 16) {
+                let character = char::from_u32(hex_value as u32).unwrap();
+                memory = String::from(&memory.replace(
+                    &memory[hex_value_index + 0..hex_value_index + 4],
+                    String::from(character).as_str(),
+                ));
             }
         }
 
