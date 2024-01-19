fe44727157f0765dfda17c809a3646cbeab1f219
Elvis Brevi <elvisbrevi@gmail.com>
2024-01-19 09:02:19 -0300

    feat: solution for part two

--- 2015/day-8/src/main.rs
+++ 2015/day-8/src/main.rs
@@ -3,28 +3,34 @@ use regex::Regex;
 fn main() {
     let contents = include_str!("../input.txt");
     let lines: Vec<&str> = contents.split_whitespace().collect();
-    let mut char_string_literals = 0;
-    let mut char_in_memory = 0;
+    let mut literal_strings = 0;
+    let mut memory_strings = 0;
+    let mut encoded_strings = 0;
 
     let regex = Regex::new(r"\\x[a-f0-9A-F]{2}").unwrap();
 
     for line in lines {
-        char_string_literals += line.len();
+        literal_strings += line.len();
 
-        char_in_memory += regex
+        memory_strings += regex
             .replace_all(line, "x")
             .replace("\\\\", "x")
             .replace("\\\"", "x")
             .len()
             - 2;
+
+        encoded_strings += &line.replace("\\", "xx").replace("\"", "xx").len() + 2;
     }
 
-    println!("{}", char_string_literals);
-    println!("{}", char_in_memory);
-    println!("{}", char_string_literals - char_in_memory);
-
-    // ""           2       0
-    // "abc"        5       3
-    // "aaa\"aaa"   10      7
-    // "\x27"       6       1
+    println!("{} literal_strings", literal_strings);
+    println!("{} memory_strings", memory_strings);
+    println!(
+        "{} literal_strings - memory_strings",
+        literal_strings - memory_strings
+    );
+    println!("{} encoded_strings", encoded_strings);
+    println!(
+        "{} encoded_strings - literal_strings",
+        encoded_strings - literal_strings
+    );
 }
