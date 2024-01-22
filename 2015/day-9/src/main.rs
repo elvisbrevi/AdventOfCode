b21c06bf459ddb08db6380caf12019672b02c90d
Elvis Brevi <elvis.brevi@sag.gob.cl>
2024-01-22 18:38:04 -0300

    feat: unfinished day 9

--- 2015/day-9/src/main.rs
+++ 2015/day-9/src/main.rs
@@ -2,19 +2,31 @@ use std::collections::HashMap;
 
 fn main() {
     let contents = include_str!("../input-test.txt");
-    let lines: Vec<&str> = contents.split("=").collect();
+    let lines: Vec<&str> = contents.split("\n").collect();
     let mut route_hashmap = HashMap::new();
 
-    // fill route_hashmap
-    for index in (0..lines.len()).step_by(2) {
-        let route = *lines.get(index).unwrap();
-        let distance = lines.get(index - 1).unwrap().parse::<i32>().unwrap();
+    for line in lines {
+        // concatenate route with it's distance
+        let split_line = line.split(" = ").collect::<Vec<&str>>();
+        let route = *split_line.get(0).unwrap();
+        let distance = split_line
+            .get(1)
+            .unwrap()
+            .replace("\r", "")
+            .parse::<i32>()
+            .unwrap();
         route_hashmap.insert(route, distance);
-    }
 
-    let destiny = String::new();
-    // search destiny
-    let search_destiny = |city: &str| {
-        for r in routes {}
-    };
+        // search destiny
+        let right_city = route.split_whitespace().last().unwrap();
+        let exist = contents.find(right_city).unwrap();
+        println!("{}, {}", right_city, exist)
+    }
+    println!("{:?}", route_hashmap)
+
+    // let destiny = String::new();
+    // // search destiny
+    // let search_destiny = |city: &str| {
+    //     for r in routes {}
+    // };
 }
