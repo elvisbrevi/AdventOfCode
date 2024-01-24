e88641e80dc9dabb60edf7c2d890859cd3b49d7b
Elvis Brevi <elvis.brevi@sag.gob.cl>
2024-01-24 15:59:58 -0300

    unfinished solution

--- 2015/day-9/src/main.rs
+++ 2015/day-9/src/main.rs
@@ -1,9 +1,10 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, u16::MAX};
 
 fn main() {
-    let contents = include_str!("../input-test.txt");
+    let contents = include_str!("../input.txt");
     let lines: Vec<&str> = contents.split("\n").collect();
     let mut route_hashmap = HashMap::new();
+    let mut destiny = String::new();
 
     for line in lines {
         // concatenate route with it's distance
@@ -19,14 +20,32 @@ fn main() {
 
         // search destiny
         let right_city = route.split_whitespace().last().unwrap();
-        let exist = contents.find(right_city).unwrap();
-        println!("{}, {}", right_city, exist)
+        let city_to_match = right_city.to_string() + " to ";
+        let only_one = contents.matches(&city_to_match).count() == 0;
+        if only_one {
+            destiny = right_city.to_string();
+        }
     }
-    println!("{:?}", route_hashmap)
 
-    // let destiny = String::new();
-    // // search destiny
-    // let search_destiny = |city: &str| {
-    //     for r in routes {}
-    // };
+    println!("{}", destiny);
+    let total_distance = get_total_distance(&destiny, &route_hashmap);
+    println!("{}", total_distance)
+}
+
+fn get_total_distance(destiny: &str, route_hashmap: &HashMap<&str, i32>) -> i32 {
+    let city_to_match = "to ".to_string() + destiny;
+    let mut min_distance = i32::MAX;
+    for (key, val) in route_hashmap {
+        if key.contains(&city_to_match) && val < &min_distance {
+            let key_cities: Vec<&str> = key.split("to").collect();
+            let left_city = key_cities.get(0).unwrap().replace(" ", "");
+            min_distance = *val;
+            min_distance = min_distance + get_total_distance(&left_city, route_hashmap);
+        }
+    }
+    if min_distance == i32::MAX {
+        return 0;
+    } else {
+        min_distance
+    }
 }
